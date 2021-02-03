205e5a75ed7f6947831a04a4707f13ace3f92c13
Douglas Tertuliano <douglasjtds@gmail.com>
2021-02-03 14:10:48 -0300

    adicionando aplicação Blog

---
+++ ProjetoBlog_V2_Por_OK/projetoBlog/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using projetoBlog.Models;
+using projetoBlog.Models.Account;
+
+namespace projetoBlog.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Acessar(string returnUrl)
+        {
+            var model = new AcessarModel
+            {
+                RetornoUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Acessar(AcessarModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var conectandoMongoDb = new AcessoMongoDB();
+            var construtor = Builders<Usuario>.Filter;
+            var condicao = construtor.Eq(x => x.Email, model.Email);
+            var user = await conectandoMongoDb.Usuarios.Find(condicao).SingleOrDefaultAsync();
+
+            // Neste ponto iremos buscar o email digitado ao acessar o Blog
+            // Descomentar as linhas abaixo
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "Correio não foi registrado.");
+                return View(model);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Nome),
+                    new Claim(ClaimTypes.Email, user.Email)
+                },
+                "ApplicationCookie");
+
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignIn(identity);
+
+            return Redirect(GetRedirectUrl(model.RetornoUrl));
+        }
+
+        [HttpPost]
+        public ActionResult Desconectar()
+        {
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View(new RegistrarModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(RegistrarModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new Usuario
+            {
+                Nome = model.Nome,
+                Email = model.Email
+            };
+
+            var conectandoMongoDb = new AcessoMongoDB();
+
+            //inserindo usuário
+            await conectandoMongoDb.Usuarios.InsertOneAsync(user);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
---
+++ ProjetoBlog_V2_Por_OK/projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+        public const string STRING_DE_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "Blog";
+        public const string NOME_DA_COLECAO_PUBLICACAO = "Publicacoes";
+        public const string NOME_DA_COLECAO_USUARIO = "Usuarios";
+
+        private static readonly IMongoClient _cliente;
+        private static readonly IMongoDatabase _baseDeDados;
+
+        static AcessoMongoDB()
+        {
+            _cliente = new MongoClient(STRING_DE_CONEXAO);
+            _baseDeDados = _cliente.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return _cliente; }
+        }
+
+        public IMongoCollection<Usuario> Usuarios
+        {
+            get { return _baseDeDados.GetCollection<Usuario>(NOME_DA_COLECAO_USUARIO); }
+        }
+
+        public IMongoCollection<Publicacao> Publicacoes
+        {
+            get { return _baseDeDados.GetCollection<Publicacao>(NOME_DA_COLECAO_PUBLICACAO); }
+        }
+    }
+}
