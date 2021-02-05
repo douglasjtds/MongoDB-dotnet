f3ea50e8834b6fb4c91311de3c1109fe4e02454f
Douglas Tertuliano <douglasjtds@gmail.com>
2021-02-05 11:43:38 -0300

    adicionando projeto Geo Airports

---
+++ aspnetmvc-gmaps-master/Alura.GoogleMaps.Web/Models/Aeroporto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Alura.GoogleMaps.Web.Models
+{
+    public class Aeroporto
+    {
+        //XXX TRABALHE AQUI
+        // Crie a classe que representa um aeroporto no MongoDB
+
+    }
+}
---
+++ aspnetmvc-gmaps-master/Alura.GoogleMaps.Web/Models/conectandoMongoDBGeo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Alura.GoogleMaps.Web.Models
+{
+    public class conectandoMongoDBGeo
+    {
+        //XXX TRABALHE AQUI
+        // Crie a classe de conexão com o MongoDB
+
+
+    }
+}
